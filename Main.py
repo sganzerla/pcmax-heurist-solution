32b01b0cb4c9af7b7b4701f0859a114fee9f9cc8
Alisson Michel <alisson.sganzerla@acad.ufsm.br>
2022-11-27 16:45:48 -0300

    chore: best strategy

--- Main.py
+++ Main.py
@@ -40,6 +40,6 @@ if __name__ == "__main__":
         ins = Instance(m, p, s)
         s, n = ins.get_S(), ins.get_N()
 
-        cons = ConstrutiveSolution(m, n, s, Strategy.BEST)
+        cons = ConstrutiveSolution(m, n, s, Strategy.FIRST)
         cons.to_string()
         aux += 1
