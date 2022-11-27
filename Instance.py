a3b2de27375600dca7ad09d68a9151a19b074258
Alisson Michel <alisson.sganzerla@acad.ufsm.br>
2022-11-27 00:01:07 -0300

    fix: instance join time + setups

--- Instance.py
+++ Instance.py
@@ -1,36 +1,32 @@
 import numpy as np
 import pandas as pd
+
+
 class Instance:
     def __init__(self, M: int, P: np.array, S: np.array):
         self.__M = M
         self.__P = P
         self.__N = len(P)
         self.__S = self.__join_times__(S)
-        
+
     def __join_times__(self, S: np.array) -> np.array:
         n = self.get_N()
         for i in range(n):
             S[i][0:n] = S[i][0:n] + self.__P[i]
         return S
-    
-    
+
     def to_string(self):
-        print(f"M: {self.__M}\nN: {self.__N}\nP: {self.__P}\nS: {pd.DataFrame(self.__S).T}")
-        
+        print(
+            f"M: {self.__M}\nN: {self.__N}\nP: {self.__P}\nS: {pd.DataFrame(self.__S).T}")
+
     def get_M(self) -> int:
         return self.__M
-    
+
     def get_N(self) -> int:
-    
         return self.__N
-    
-    def get_P(self, i)-> int:
-        return self.P[i] 
-    
+
+    def get_P(self, i) -> int:
+        return self.P[i]
+
     def get_S(self, i, j) -> int:
         return self.__S[i][j]
-    
-    
-        
-    
-
