d280f05418b007fac3c2c8bb0965c8cd9abe5cfa
Alisson Michel <alisson.sganzerla@acad.ufsm.br>
2022-11-30 00:48:15 -0300

    refactor: utilizando enum para representar posicao do no: predecessor ou sucessor

--- Solution.py
+++ Solution.py
@@ -1,5 +1,11 @@
 import numpy as np
 from Instance import *
+from enum import IntEnum
+
+
+class Node(IntEnum):
+    Pre = 0
+    Suc = 1
 
 
 class Solution:
@@ -8,8 +14,8 @@ class Solution:
         self.mj = -np.ones(inst.get_N(), dtype=int)
         self.m = -np.ones((2, inst.get_N()+inst.get_M()), dtype=int)
         for i in range(0, inst.get_M()):
-            self.m[0][inst.get_N()+i] = inst.get_N()+i
-            self.m[1][inst.get_N()+i] = inst.get_N()+i
+            self.m[Node.Pre][inst.get_N()+i] = inst.get_N()+i
+            self.m[Node.Suc][inst.get_N()+i] = inst.get_N()+i
         self.Cmax = 0
         self.i_Cmax = -1
         self.C = np.zeros(inst.get_M(), dtype=int)
@@ -18,8 +24,8 @@ class Solution:
         self.mj = -np.ones(self.inst.get_N(), dtype=int)
         self.m = -np.ones((2, self.inst.get_N()+self.inst.get_M()), dtype=int)
         for i in range(0, self.inst.get_M()):
-            self.m[0][self.inst.get_N()+i] = self.inst.get_N()+i
-            self.m[1][self.inst.get_N()+i] = self.inst.get_N()+i
+            self.m[Node.Pre][self.inst.get_N() + i] = self.inst.get_N() + i
+            self.m[Node.Suc][self.inst.get_N() + i] = self.inst.get_N() + i
         self.Cmax = 0
         self.i_Cmax = -1
         self.C = np.zeros(self.inst.get_M(), dtype=int)
@@ -31,10 +37,10 @@ class Solution:
         return self.C[m]
 
     def get_suc(self, job: int) -> int:
-        return self.m[1][job]
+        return self.m[Node.Suc][job]
 
     def get_pre(self, job: int) -> int:
-        return self.m[0][job]
+        return self.m[Node.Pre][job]
 
     def get_job_machine(self, job: int) -> int:
         return self.mj[job]
@@ -45,10 +51,10 @@ class Solution:
     def to_string(self):
         for i in range(self.inst.get_M()):
             print(f"M{i} : ", end="")
-            fj = self.m[1][self.inst.get_N()+i]
+            fj = self.m[Node.Suc][self.inst.get_N()+i]
             while fj < self.inst.get_N():
                 print(fj, end=" ")
-                fj = self.m[1][fj]
+                fj = self.m[Node.Suc][fj]
             print('\nC : ', self.C[i])
         print('\nCmax : ', self.Cmax)
 
@@ -57,9 +63,9 @@ class Solution:
             self.inst.get_S(pre, self.m[1][pre])
         self.C[index_m] += self.inst.get_S(job, self.m[1][pre])
         suc = self.m[1][pre]
-        self.m[1][pre] = job
-        self.m[0][job] = pre
-        self.m[1][job] = suc
+        self.m[Node.Suc][pre] = job
+        self.m[Node.Pre][job] = pre
+        self.m[Node.Suc][job] = suc
 
         if self.C[index_m] > self.Cmax:
             self.Cmax = self.C[index_m]
