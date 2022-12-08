a85f83e48802a689ebb07d2611dfb9fc249a2a6e
Alisson Michel Sganzerla <alisson.sganzerla@acad.ufsm.br>
2022-12-08 14:32:35 -0300

    chore: unica instancia

--- Main.py
+++ Main.py
@@ -5,14 +5,6 @@ from ConstructiveSolution import *
 from LocalSearch import *
 
 
-def get_path_files(path_root: str):
-    path_files = []
-    for (_, _, path_file) in os.walk(path_root):
-        for f in path_file:
-            path_files.append(f)
-    return path_files
-
-
 if __name__ == "__main__":
 
     # python.exe .\Main.py -s .\instance_one\
@@ -26,40 +18,32 @@ if __name__ == "__main__":
     path = opts.path
 
     # add path root com inst
-    files = get_path_files(path)
 
-    # barra de progress
-    n_files = len(files)
-    aux = 1
 
-    for file in files:
-        print(f"({aux}/{n_files})")
+    ex = Extract(path)
+    inst = Instance(ex)
+    solu = Solution(inst)
 
-        ex = Extract(path + file)
-        inst = Instance(ex)
-        solu = Solution(inst)
-
-        greedy = ConstructiveSolution(inst)
-        ls = LocalSearch(inst)
-        # greedy.build_naive(solu)
-        # solu.to_string()
-        # solu.check_fact()
-        # solu.check_solution()
-        # solu.reset()
-        
-        # inst.to_string()
-        greedy.build_greedy(solu)
-        solu.to_string()
-        solu.check_solution()
-        print("================ Swap      =============") 
-        ls.swap(solu)        
-        solu.to_string()
-        solu.check_solution() 
-        print("================ Insertion =============") 
-        ls.insertion(solu)        
-        solu.to_string()
-        solu.check_solution() 
-        
-        aux += 1
-        exit(0)
+    greedy = ConstructiveSolution(inst)
+    ls = LocalSearch(inst)
+    # greedy.build_naive(solu)
+    # solu.to_string()
+    # solu.check_fact()
+    # solu.check_solution()
+    # solu.reset()
+    
+    # inst.to_string()
+    greedy.build_greedy(solu)
+    solu.to_string()
+    solu.check_solution()
+    print("================ Swap      =============") 
+    ls.swap(solu)        
+    solu.to_string()
+    solu.check_solution() 
+    print("================ Insertion =============") 
+    ls.insertion(solu)        
+    solu.to_string()
+    solu.check_solution() 
+    
+    exit(0)
 
