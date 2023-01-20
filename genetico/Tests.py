ab3b2a57fe374bf5d44f2ba32f30b825be8c5098
Alisson <alisson.sganzerla@acad.ufsm.br>
2023-01-20 11:21:50 -0300

    chore: relatório ok

--- genetico/Tests.py
+++ genetico/Tests.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     # python3 Tests.py --source instance/ --repeat 9 --greedy 0
     # python3 Tests.py -s instance/ -r 10 -g 1
 
-    gen_size = 5  # quantidade de gerações
+    gen_size = 10  # quantidade de gerações
     repeat_size, root, greedy_type = get_root_instances()
 
     files = []
@@ -54,18 +54,16 @@ if __name__ == "__main__":
 
     inst_size = len(files)  # quantidade de instâncias dentro da pasta
     files = sorted(files)
-
     # extrai dados da literatura
     lit_sols: List[ExtractSolution] = np.ndarray(inst_size, dtype=ExtractSolution)
     for i in range(inst_size):
         lit_sol = ExtractSolution(files[i], os.path.join("solution/" + files[i]))
         lit_sols[i] = lit_sol
-
     times = np.ndarray(repeat_size, dtype=float)
     cmaxs = np.ndarray(repeat_size, dtype=int)
     gaps = np.ndarray(repeat_size, dtype=float)
-    data = pd.DataFrame(columns=["instance", "m", "n", "repeat", "population", "generation", "cmax_const",
-                        "cmax_genet", "time", "gap",  "var_cmax", "std_cmax", "mean_cmax", "var_time", "std_time", "mean_time", "use_const_init_sol", "mutation_size"])
+    data = pd.DataFrame(columns=["instance", "m", "n", "repeat", "population", "generation", "literature",
+                        "cmax", "time", "gap",  "var_cmax", "std_cmax", "mean_cmax", "var_time", "std_time", "mean_time", "use_const_init_sol", "mutation_size"])
     for i in range(inst_size):
         file = files[i]
         name = "_".join(str(k) for k in file.split("_")[2:])
@@ -83,6 +81,7 @@ if __name__ == "__main__":
                 sol = Solution(inst)
                 constr.build_naive(sol)
                 init_pop[k] = sol
+            
             # relatorio inclui solução gulosa dentro pop inicial
             if greedy_type == 1:
                 init_pop[0] = greedy_sol
@@ -96,7 +95,7 @@ if __name__ == "__main__":
             cmaxs[j] = ga.inc_sol.cmax
             gaps[j] = ((lit_sol.cmax - ga.inc_sol.cmax) / ga.inc_sol.cmax) * -1
             print(
-                f"inst: {name} |  m: {inst.get_m()} | n: {inst.get_n()} | repeat: {j} | pop_size: {pop_size} | generations: {gen_size} | cmax_lit: {lit_sol.cmax} |  cmax_genetic: {cmaxs[j]} | time: {times[j]:.2f} | gap_lit: {gaps[j]:.2f} | start_good_sol: {use_const_init_sol(greedy_type)} | mutations: {int(0.10 * pop_size + 1)}")
+                f"inst: {name} |  m: {inst.get_m()} | n: {inst.get_n()} | repeat: {j} | pop_size: {pop_size} | generations: {gen_size} | literature: {lit_sol.cmax} |  cmax: {cmaxs[j]} | time: {times[j]:.2f} | gap: {gaps[j]:.2f} | start_good_sol: {use_const_init_sol(greedy_type)} | mutations: {int(0.10 * pop_size + 1)}")
         var_time = np.var(times)
         mean_time = np.mean(times)
         std_time = np.std(times)
@@ -109,9 +108,9 @@ if __name__ == "__main__":
             "instance": np.array([name]*repeat_size),
             "m": np.array([inst.get_m()] * repeat_size),
             "n": np.array([inst.get_n()] * repeat_size),
-            "cmax_const": np.array([greedy_sol.cmax] * repeat_size),
+            "literature": np.array([lit_sol.cmax] * repeat_size),
             "repeat": range(repeat_size),
-            "cmax_genet": cmaxs,
+            "cmax": cmaxs,
             "time": times,
             "var_cmax": np.array([var_cmax] * repeat_size),
             "std_cmax": np.array([std_cmax] * repeat_size),
@@ -123,7 +122,7 @@ if __name__ == "__main__":
             "population": np.array([pop_size] * repeat_size),
             "gap": gaps,
             "use_const_init_sol": use_good_init_sol,
-            "mutations": np.array([int(0.10 * pop_size + 1)] * repeat_size)
+            "mutation_size": np.array([int(0.10 * pop_size + 1)] * repeat_size)
         })
 
         data = pd.concat([data, df], ignore_index=True)
