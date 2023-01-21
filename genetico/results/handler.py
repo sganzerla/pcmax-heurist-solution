e3174ca5f9e450cfeaf1134e263f2978cece5177
Alisson <alisson.sganzerla@acad.ufsm.br>
2023-01-21 07:46:28 -0300

    chore: classe para gerar relatórios

--- genetico/results/handler.py
+++ genetico/results/handler.py
@@ -1,32 +1,43 @@
 import pandas as pd
 import os
 
-# uninformed initial solution
-u_is = pd.read_csv(os.path.join("amd", "report0.csv"))
-# greedy initial solution
-# g_is = 
-# literary initial solution
-# l_is =
+
+class BuildPandas:
+    def __init__(self, path: str ):
+        self.df: pd.DataFrame = pd.read_csv(path)
+        self.df_report_cmax: pd.DataFrame
+        self.build_report_cmax()
+        
+
+    def build_report_cmax(self):
+        df_gr = self.df.groupby(['struct','instance', 'literature'], sort=False, as_index=False)['cmax'].aggregate({'Min':'min', 'Max':'max', 'Mean': 'mean', 'Std': 'std', 'Var': 'var'})
+        df_gr['Gap Min'] = self.__calc_gap_min__(df_gr, 'literature', 'Min')
+        df_gr['Gap Mean'] = self.__calc_gap_min__(df_gr, 'literature', 'Mean')
+        # deixar float com 2 decimais
+        df_gr: pd.DataFrame = df_gr.round(2)
+        # começa o indice com 1
+        df_gr.index +=1
+        
+        self.df_report_cmax = df_gr
+        
+    def __calc_gap_min__(self, df_gr: pd.DataFrame, col_a: str, col_b: str) -> pd.Series:
+        col = (df_gr[col_a] - df_gr[col_b]) / df_gr[col_b]
+        col =  col.apply(lambda x: x * -100 if float(x) != 0.0 else x )
+        col = col.apply(lambda x: str(f"{x:.2f}%"))
+        return col
+
+    def get_latex(self):
+        print(self.df_report_cmax.to_latex(header=["Struct", "Instance", "C(BKS)", "C(Min)", "C(Max)", "C(Mean)", "C(Std)", "C(Var)", "Gap(Min)", "Gap(Mean)"]))
 
 
-# u_is.reset_index(drop=True)
-# print(u_is.head(5))
-u_is_group = u_is.groupby(['struct','instance', 'literature'], sort=False, as_index=False)['cmax'].aggregate({'Min':'min', 'Max':'max', 'Mean': 'mean', 'Std': 'std', 'Var': 'var'})
-
-# reduzir casas decimais
-
-
-def calc_gap_min(u_is_group: pd.DataFrame, col_a: str, col_b: str) -> pd.Series:
-    col = (u_is_group[col_a] - u_is_group[col_b]) / u_is_group[col_b]
-    col =  col.apply(lambda x: x * -100 if float(x) != 0.0 else x )
-    col.round(2)
-    col = col.apply(lambda x: str(f"{x:.2f}%"))
-    return col
-
-u_is_group['Gap Min'] = calc_gap_min(u_is_group, 'literature', 'Min')
-u_is_group['Gap Mean'] = calc_gap_min(u_is_group, 'literature', 'Mean')
-
-u_is_group = u_is_group.round(2)
-
-print(u_is_group)
-
+if __name__ == "__main__":
+    
+    # uninformed initial solution
+    u_is = os.path.join("amd", "report0.csv")
+    # greedy initial solution
+    g_is = os.path.join("amd", "report1.csv")
+    # literary initial solution
+    l_is = os.path.join("amd", "report2.csv")
+    
+    u = BuildPandas(g_is)
+    u.get_latex()
